3b60b50a2633efd46475205eafd35c56a69abf81
Aarit1 <85659004+Aarit1@users.noreply.github.com>
2022-02-23 17:37:12 -0800

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,10 @@
+ var firebaseConfig = {
+    apiKey: "AIzaSyDOj-2rPST0ZnxUBD4JuSPNcTL3mH8DXo8",
+    authDomain: "lets-chat-1fdb1.firebaseapp.com",
+    databaseURL: "https://lets-chat-1fdb1-default-rtdb.firebaseio.com",
+    projectId: "lets-chat-1fdb1",
+    storageBucket: "lets-chat-1fdb1.appspot.com",
+    messagingSenderId: "534257675144",
+    appId: "1:534257675144:web:b8df191a5cf390a45ca83d"
+  };
+  firebase.initializeApp(firebaseConfig);
